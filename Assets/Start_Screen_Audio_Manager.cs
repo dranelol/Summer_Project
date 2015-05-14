f5881e4ab15033b5679381d74d3beb6a91f2a104
skyflyer55 <jlecoq@eatel.net>
2015-05-13 19:15:32 -0500

    asd

---
+++ Assets/Start_Screen_Audio_Manager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Start_Screen_Audio_Manager : MonoBehaviour{
+    AudioSource currentSource;
+	// Use this for initialization
+	void Start () {
+        currentSource = GetComponent<AudioSource>();
+        Utilites.Audio_Lead_In(currentSource);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
---
+++ Assets/Utilites.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Utilites{
+	public static void Audio_Lead_In(AudioSource source){
+        source.volume = 0;
+		while(source.volume <= 1){
+			source.volume += Time.deltaTime;
+		}
+	}
+}
